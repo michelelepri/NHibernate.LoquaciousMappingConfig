bd1c6d441512c1f70dfa9b7c5ef3aadc7f56fa58
Michele Lepri <michele.lepri@gmail.com>
2012-01-31 21:06:28 +0100

    Added overload to get assemblies loaded

--- ModelMapperExtend.cs
+++ ModelMapperExtend.cs
@@ -18,7 +18,17 @@
       /// <returns></returns>
       public static ModelMapper AddFromConfig(this ModelMapper modelMapper)
       {
-         return AddFromConfig(modelMapper, "LoquaciousNHibernateMapping");
+         return AddFromConfig(modelMapper, "loquaciousNHibernateMapping");
+      }
+      /// <summary>
+      /// Add the mapping from the "fluentNHibernateMapping" config section. For now can be added only the assemblies
+      /// </summary>
+      /// <param name="modelMapper"></param>
+      /// <param name="assemblies">return the array of the assemblyes loaded with the maps</param>
+      /// <returns></returns>
+      public static ModelMapper AddFromConfig(this ModelMapper modelMapper, out Assembly[] assemblies)
+      {
+         return AddFromConfig(modelMapper, "loquaciousNHibernateMapping", out assemblies);
       }
 
       /// <summary>
@@ -30,19 +40,37 @@
       /// <exception cref="ApplicationException"></exception>
       public static ModelMapper AddFromConfig(this ModelMapper modelMapper, string configSection)
       {
+         Assembly[] assemblies;
+
+         return AddFromConfig(modelMapper, configSection, out assemblies);
+      }
+
+      /// <summary>
+      /// Add the mapping from a config section. For now can be added only the assemblies
+      /// </summary>
+      /// <param name="modelMapper"></param>
+      /// <param name="configSection"></param>
+      /// <param name="assemblies">return the array of the assemblyes loaded with the maps</param>
+      /// <returns></returns>
+      /// <exception cref="ApplicationException"></exception>
+      public static ModelMapper AddFromConfig(this ModelMapper modelMapper, string configSection, out Assembly[] assemblies)
+      {
          var ris = (LoquaciousNHibernateMappingSection)ConfigurationManager.GetSection(configSection);
 
          if (ris == null)
          {
-            string msg = string.Format("Failed to load the config section \"{0}\" for the LoquaciousNHibernateMapping.",
+            string msg = string.Format("Failed to load the config section \"{0}\" for the loquaciousNHibernateMapping.",
                                        configSection);
             throw new ApplicationException(msg);
          }
 
+         assemblies = new Assembly[ris.Assemblies.Count];
          for (int i = 0; i < ris.Assemblies.Count; ++i)
          {
             var module = ris.Assemblies[i];
-            modelMapper.AddMappings(Assembly.Load(module.Assembly).GetTypes());
+            var asm = Assembly.Load(module.Assembly);
+            assemblies[i] = asm;
+            modelMapper.AddMappings(asm.GetTypes());
          }
 
          return modelMapper;
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NHibernate.LoquaciousMappingConfig")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("http://michelelepri.blogspot.com/")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NHibernate.LoquaciousMappingConfig")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyCopyright("Copyright Michele Lepri 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
